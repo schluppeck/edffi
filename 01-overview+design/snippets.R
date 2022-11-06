6cfe79c8f86fe41d49b976979a1f8b29adea5974
Denis Schluppeck <denis.schluppeck@gmail.com>
2022-11-06 16:44:17 +0000

    commit lecture 1 materials

---
+++ 01-overview+design/snippets.R
@@ -0,0 +1,27 @@
+##  {auto-animate="true" auto-animate-easing="ease-in-out"}
+
+::: r-hstack
+::: {data-id="box1" auto-animate-delay="0" style="background: #2780e3; opacity: 0.2; width: 200px; height: 150px; margin: 10px; text-align: center;"}
+[bla]{style="color: white; "}
+:::
+  
+  ::: {data-id="box2" auto-animate-delay="0.1" style="background: #3fb618; width: 200px; height: 150px; margin: 10px;"}
+:::
+  
+  ::: {data-id="box3" auto-animate-delay="0.2" style="background: #e83e8c; width: 200px; height: 150px; margin: 10px;"}
+:::
+  :::
+  
+  ##  {auto-animate="true" auto-animate-easing="ease-in-out"}
+  
+  ::: r-hstack
+::: {data-id="box1" auto-animate-delay="0" style="background: #2780e3; width: 200px; height: 150px; margin: 10px;"}
+[bla]{style="color: white; text-align: center;"}
+:::
+  
+  ::: {data-id="box2" auto-animate-delay="0.1" style="background: #3fb618; width: 200px; height: 150px; margin: 10px;"}
+:::
+  
+  ::: {data-id="box3" auto-animate-delay="0.2" style="background: #e83e8c; width: 200px; height: 150px; margin: 10px;"}
+:::
+:::
